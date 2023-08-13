89ac330b3bba76b5d6ef14bee384d65c5835a12a
arun <you@example.com>
2023-08-13 16:47:03 +0530

    second commit

---
+++ src/main/kotlin/course/week2/OpenClassesAndMethods.kt
@@ -0,0 +1,16 @@
+package course.week2
+
+//When one class inherits from another, it takes all the previous class’s behavior (its methods and properties).
+// It also becomes its subclass, which means that it can be used wherever its parent is expected
+
+open class Mammal {
+    fun feedCildren(){
+
+    }
+}
+
+class Dog: Mammal {
+    fun fetchStick(){
+
+    }
+}
